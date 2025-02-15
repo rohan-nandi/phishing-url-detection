2b4877de048c4c22a34d3d96ef200a088d592c6a
Rohan Nandi <nandi.rohan@yahoo.com>
2025-02-15 12:54:20 +0530

    Added port mapping for Flask

--- flask-api-script.py
+++ flask-api-script.py
@@ -121,4 +121,4 @@ def predict():
     return jsonify({"url": url, "result": result})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
